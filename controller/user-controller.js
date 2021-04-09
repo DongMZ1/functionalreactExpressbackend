5264cec44e4baffa048d11d954fc5eefc6fa211f
DongMZ1 <915094594@qq.com>
2021-04-08 23:13:13 -0400

    add login router

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -49,4 +49,48 @@ const signup = async (req, res, next) =>{
 
 }
 
+
+const login = async (req, res, next) =>{
+
+  const { email, password } = req.body;
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!existingUser) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      403
+    );
+    return next(error);
+  }
+
+
+  if(password !== existingUser.password){
+    const error = new HttpError(
+      'Password is not valid, please try again',
+      403
+    );
+    return next(error);
+  }
+
+  const token = jwt.sign({email: email}, "secret_email");
+  res
+  .status(201)
+  .json({ email: existingUser.email, token: token});
+
+
+  
+}
+
 exports.signup = signup;
+exports.login = login;
--- router/user-router.js
+++ router/user-router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { check } = require('express-validator');
 
-const {signup} = require('../controller/user-controller');
+const {signup, login} = require('../controller/user-controller');
 
 const router = express.Router();
 router.post('/signup', signup);
+router.post('/login', login);
 
 module.exports = router;
