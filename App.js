f5d49be939e9b457b6bffd30c4ffd424deb45f20
DongMZ1 <915094594@qq.com>
2021-04-28 23:19:44 -0400

    allow token as part of header

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, token");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   
     next();
