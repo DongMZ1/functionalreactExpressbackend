492aae8a42e9902e86b5ba71a6fdb787590fd643
DongMZ1 <915094594@qq.com>
2021-04-28 23:17:33 -0400

    try to fix cor error

--- App.js
+++ App.js
@@ -12,11 +12,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   
     next();
